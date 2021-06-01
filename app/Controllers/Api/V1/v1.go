d82ec7034fc64559cf9b0bbef73ed804d5942935
zhuchunshu <laravel@88.com>
2021-06-01 13:59:22 +0800

    更新:V1-QQHOOK功能实现

--- app/Controllers/Api/V1/v1.go
+++ app/Controllers/Api/V1/v1.go
@@ -8,9 +8,27 @@ package V1
 
 import (
 	"github.com/gofiber/fiber/v2"
+	database2 "github.com/zhuchunshu/FecApi-Api/helpers/database"
 	"github.com/zhuchunshu/FecApi-Api/helpers/response"
+	"net/http"
+	"net/url"
 )
 
 func TestPage(ctx *fiber.Ctx)error{
-	return ctx.JSON(response.StringApi(200,true,"test"))
+	token:=database2.GetOptions("api-v1-qqhook-token")
+	urls:=database2.GetOptions("api-v1-qqhook-url")
+	if len(token)!=0 && len(urls)!=0 {
+		qq :=ctx.Query("qq")
+		text:=ctx.Query("text")
+		if len(qq)==0 {
+			return ctx.JSON(response.StringApi(200, true, "缺少参数: qq"))
+		}
+		if len(text)==0 {
+			return ctx.JSON(response.StringApi(200, true, "缺少参数: text"))
+		}
+		http.PostForm(urls+token+"/private/"+qq+"/"+text,url.Values{})
+	}else {
+		return ctx.JSON(response.StringApi(404,false,"此接口服务端配置不完整"))
+	}
+	return ctx.JSON(response.StringApi(200,true,"1"))
 }
